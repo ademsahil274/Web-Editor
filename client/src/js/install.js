39c6d64ed36e4f2e92f6445ad23196cdb66e68af
Adem Sahil <adem.sahil274@gmail.com>
2023-02-21 20:48:19 -0500

    src-sw, webpack, src_all the js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,7 +5,34 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    // Store the triggered events
+    window.deferredPrompt = Event;
+
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
+});
+butInstall.addEventListener('click', async () => {
+  
+    const promptEvent = window.deferredPrompt;
+  
+    if (!promptEvent) {
+     return;
+    }
+  
+    // Show prompt
+    promptEvent.prompt();
+    
+    // Reset the deferred prompt variable, it can only be used once.
+    window.deferredPrompt = null;
+    
+    butInstall.classList.toggle('hidden', true);
+  });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // Clear prompt
+  window.deferredPrompt = null;
+
+}); 
+
